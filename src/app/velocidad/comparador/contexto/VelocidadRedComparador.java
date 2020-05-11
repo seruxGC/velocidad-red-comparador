bb1a8138ed86bae71e1cb9887df1e8daffcf2bfe
Serux <serux91@gmail.com>
2020-05-11 20:22:25 +0200

    Refactoriza

--- src/app/velocidad/comparador/contexto/VelocidadRedComparador.java
+++ src/app/velocidad/comparador/contexto/VelocidadRedComparador.java
@@ -12,17 +12,15 @@ import app.velocidad.comparador.interfaces.ComparacionVelocidadStrategy;
 
 public class VelocidadRedComparador {
 
-    // Mapa que contiene todos los patrones de velocidad y la estrategia a utilizar para cada uno
+    /**
+     * Mapa que contiene todos los patrones de velocidad y la estrategia a utilizar
+     * para cada uno
+     */
     private static final Map<Pattern, ComparacionVelocidadStrategy> mapaEstrategias;
-    
+
     static {
         Map<Pattern, ComparacionVelocidadStrategy> tempMap = new HashMap<>();
 
-        /**
-         * TODO cambiar el lugar de instanciacion y dejar en el mapa solo el nombre de
-         * la clase a isntanciar para pasarle como argumentos la velocidad 1 y 2 por el
-         * constructor
-         */
         tempMap.put(Formato1Strategy.PATTERN, new Formato1Strategy());
         tempMap.put(Formato2Strategy.PATTERN, new Formato2Strategy());
 
@@ -33,58 +31,47 @@ public class VelocidadRedComparador {
         throw new IllegalStateException("Clase de utilidad");
     }
 
-
     public static boolean primeraVelocidadEsMayor(String velocidadRed1, String velocidadRed2) {
 
-        ComparacionVelocidadStrategy estrategiaVelocidad1 = escogeEstrategia(velocidadRed1);
-        ComparacionVelocidadStrategy estrategiaVelocidad2 = escogeEstrategia(velocidadRed2);
+        ComparacionVelocidadStrategy estrategiaVelocidad = escogeEstrategia(velocidadRed1, velocidadRed2);
 
-        if (!velocidadesTienenMismoFormato(estrategiaVelocidad1, estrategiaVelocidad2)) {
-            throw new IllegalArgumentException("las velocidades comparadas no tienen el mismo formato");
-        }
-
-        return estrategiaVelocidad1.primeraVelocidadEsMayor(velocidadRed1, velocidadRed2);
+        return estrategiaVelocidad.primeraVelocidadEsMayor(velocidadRed1, velocidadRed2);
     }
 
     public static boolean velocidadesSonIguales(String velocidadRed1, String velocidadRed2) {
 
-        ComparacionVelocidadStrategy estrategiaVelocidad1 = escogeEstrategia(velocidadRed1);
-        ComparacionVelocidadStrategy estrategiaVelocidad2 = escogeEstrategia(velocidadRed2);
+        ComparacionVelocidadStrategy estrategiaVelocidad = escogeEstrategia(velocidadRed1, velocidadRed2);
 
-        if (!velocidadesTienenMismoFormato(estrategiaVelocidad1, estrategiaVelocidad2)) {
-            throw new IllegalArgumentException("las velocidades comparadas no tienen el mismo formato");
-        }
-
-        return estrategiaVelocidad1.velocidadesSonIguales(velocidadRed1, velocidadRed2);
-    }
-
-    private static boolean velocidadesTienenMismoFormato(ComparacionVelocidadStrategy estrategiaVelocidad1,
-            ComparacionVelocidadStrategy estrategiaVelocidad2) {
-        return estrategiaVelocidad1.getClass().getCanonicalName()
-                .equals(estrategiaVelocidad2.getClass().getCanonicalName());
+        return estrategiaVelocidad.velocidadesSonIguales(velocidadRed1, velocidadRed2);
     }
 
     /**
      * Busca en el mapa de estrategias si hay alguna que cumple con uno de los
-     * patrones de velocidad implementados y en caso de que lo encuentre devuelve la
-     * estrategia a utilizar
+     * patrones de velocidad implementados
      * 
-     * @param velocidadRed
+     * @param velocidadRed1
      * @return Devuelve la estrategia a utilizar o IllegalArgumentException en caso
      *         de que no encuentre ninguna.
      */
-    private static ComparacionVelocidadStrategy escogeEstrategia(String velocidadRed) {
+    private static ComparacionVelocidadStrategy escogeEstrategia(String velocidadRed1, String velocidadRed2) {
 
         for (Map.Entry<Pattern, ComparacionVelocidadStrategy> estrategia : mapaEstrategias.entrySet()) {
-            Pattern patron = estrategia.getKey();
-            Matcher matcher = patron.matcher(velocidadRed);
 
-            if (matcher.matches()) {
+            // Obtenemos el patron de la key del mapa
+            Pattern patron = estrategia.getKey();
+
+            Matcher matcher1 = patron.matcher(velocidadRed1);
+            Matcher matcher2 = patron.matcher(velocidadRed2);
+
+            // Si el formato de ambas velocidades coinciden con el patron de la estrategia
+            // la usamos
+            if (matcher1.matches() && matcher2.matches()) {
                 return estrategia.getValue();
             }
         }
 
-        throw new IllegalArgumentException("El formato de Velocidad '" + velocidadRed + "' no esta implementado");
+        throw new IllegalArgumentException(
+                "Estrategia no implementada para  '" + velocidadRed1 + "' y  '" + velocidadRed2 + "'");
     }
 
 }
